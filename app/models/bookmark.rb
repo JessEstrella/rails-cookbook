7a621a990e535725f253adef6c590fb9d72e775b
jessica estrella <jess.m.estrella@gmail.com>
2025-05-12 11:28:26 +0100

    Models Validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,7 @@
 class Bookmark < ApplicationRecord
   belongs_to :recipe
   belongs_to :category
+
+  validates :comment, lenght: { minimum: 6 }
+  validates :recipe_id, uniqueness: { scope: :category_id, message: "has already been added to this category" }
 end
